9b1cf58c3d0dc0cbc5b8d085e01b658c9aadda81
imseoyeong <fearless40@naver.com>
2025-04-02 16:04:01 +0900

    Product.js 레이아웃 / productListSilce 작업중

--- shopping_project/src/Product.js
+++ shopping_project/src/Product.js
@@ -5,12 +5,13 @@ export default function Product() {
     const productList = useSelector((state) => state.shopping.productList);
     const dispatch = useDispatch();
 
-    const list=[];
+    const list = [];
     for (let product of productList){
         const item = <li key={product.id}>{
                 product.title} - {product.price}원
                 {/* 저장을 cart에 해야하는뎀 어케하지?^^ */}
-                <button type='button' onClick={() => dispatch(onSave({ id: product.id, title: product.title, price: product.price }))}>Add to Cart</button>
+                <button type='button'>Add to Cart</button>
+                {/* <button type='button' onClick={() => dispatch(onSave({ id: product.id, title: product.title, price: product.price }))}>Add to Cart</button> */}
             </li>;
         list.push(item);
     }
